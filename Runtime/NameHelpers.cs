5ca38a8b6fed759aa835f24dc0eb62bd0d654c2b
Jeff Boenig <jeffrey.boenig@garmin.com>
2021-08-17 07:49:06 -0400

    [master]: Fix issue with NameHelpers.GetName()

--- Runtime/NameHelpers.cs
+++ Runtime/NameHelpers.cs
@@ -33,22 +33,30 @@ namespace Headway.Dynamo.Runtime
 	public static class NameHelpers
 	{
         /// <summary>
+        /// Default delimiter used to separate names and namespaces.
+        /// </summary>
+        public const char DEFAULT_DELIMITER = '.';
+
+        /// <summary>
         /// Gets the name portion of a fully qualified name.
         /// </summary>
         /// <param name="fullName">
         /// Fully qualified name.
         /// </param>
+        /// <param name="delimiter">
+        /// Delimiter used to separate names and namespaces.
+        /// </param>
         /// <returns>
         /// Name portion (sans namespace) of the fully qualified name.
         /// </returns>
-		public static string GetName(string fullName)
+		public static string GetName(string fullName, char delimiter = DEFAULT_DELIMITER)
 		{
-			var delimiter = fullName.LastIndexOf('.');
-			if (delimiter > 0 && delimiter < fullName.Length - 1)
+			var delimiterIdx = fullName.LastIndexOf(delimiter);
+			if (delimiterIdx > 0 && delimiterIdx < fullName.Length - 1)
 			{
-				return fullName.Substring(delimiter + 1);
+				return fullName.Substring(delimiterIdx + 1);
 			}
-			return null;
+			return fullName;
 		}
 
         /// <summary>
@@ -57,15 +65,18 @@ namespace Headway.Dynamo.Runtime
         /// <param name="fullName">
         /// Fully qualified name.
         /// </param>
+        /// <param name="delimiter">
+        /// Delimiter used to separate names and namespaces.
+        /// </param>
         /// <returns>
         /// Namespace portion (sans name) of the fully qualified name.
         /// </returns>
-		public static string GetNamespace(string fullName)
+		public static string GetNamespace(string fullName, char delimiter = DEFAULT_DELIMITER)
 		{
-			var delimiter = fullName.LastIndexOf('.');
-			if (delimiter > 0)
+			var delimiterIdx = fullName.LastIndexOf(delimiter);
+			if (delimiterIdx > 0)
 			{
-				return fullName.Substring(0, delimiter);
+				return fullName.Substring(0, delimiterIdx);
 			}
 			return null;
 		}
@@ -80,17 +91,20 @@ namespace Headway.Dynamo.Runtime
         /// <param name="name">
         /// Name for the fully qualified name.
         /// </param>
+        /// <param name="delimiter">
+        /// Delimiter used to separate names and namespaces.
+        /// </param>
         /// <returns>
         /// Fully qualified name that concatenates the namespace
         /// and name using the correct delimiter.
         /// </returns>
-		public static string CreateFullName(string nameSpace, string name)
+		public static string CreateFullName(string nameSpace, string name, char delimiter = DEFAULT_DELIMITER)
 		{
 			var sb = new StringBuilder();
 			sb.Append(nameSpace);
-			if (!nameSpace.EndsWith("."))
+			if (!nameSpace.EndsWith(delimiter))
 			{
-				sb.Append(".");
+				sb.Append(delimiter);
 			}
 			sb.Append(name);
 			return sb.ToString();
@@ -102,13 +116,16 @@ namespace Headway.Dynamo.Runtime
         /// <param name="fullName">
         /// Fully qualified name to parse.
         /// </param>
+        /// <param name="delimiter">
+        /// Delimiter used to separate names and namespaces.
+        /// </param>
         /// <returns>
         /// Array of strings containing each part of the
         /// fully qualified name.
         /// </returns>
-        public static string[] ParseName(string fullName)
+        public static string[] ParseName(string fullName, char delimiter = DEFAULT_DELIMITER)
         {
-            return fullName.Split(new char[] { '.' });
+            return fullName.Split(new char[] { delimiter });
         }
 	}
 }
